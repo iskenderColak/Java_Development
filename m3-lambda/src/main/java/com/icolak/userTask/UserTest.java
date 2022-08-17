920c36ab91ee6973c4afcfb85e8a50f868f4ed18
Iskender Colak <iclk84@gmail.com>
2022-08-17 12:59:48 +0300

    LambdaPredicateUserTask

---
+++ m3-lambda/src/main/java/com/icolak/userTask/UserTest.java
@@ -0,0 +1,30 @@
+package com.icolak.userTask;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+
+        List<User> users = new ArrayList<>();
+        users.add(new User("Mike", "Smith"));
+        users.add(new User("Tom", "Evan"));
+        users.add(new User("Emma", "Pollard"));
+
+        printName(users, user -> true);
+        System.out.println("=============StartsWith::'E'=========================");
+        printName(users, user -> user.getLastname().startsWith("E"));
+    }
+
+
+
+    private static void printName(List<User> users, Predicate<User> p) {
+        for (User user : users) {
+            if (p.test(user)) {
+                System.out.println(user.toString());
+            }
+        }
+    }
+}
