9ddfeeef88af3bdf90e4f6ca38049fded6c1af17
Iskender Colak <iclk84@gmail.com>
2022-08-18 10:45:37 +0300

    Consumer Interface

--- m3-lambda/src/main/java/com/icolak/FunctionalInterface.java
+++ m3-lambda/src/main/java/com/icolak/FunctionalInterface.java
@@ -1,5 +1,6 @@
 package com.icolak;
 
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class FunctionalInterface {
@@ -8,5 +9,10 @@ public class FunctionalInterface {
         System.out.println("************PREDICATE***************");
         Predicate<Integer> lesserThan = i -> i < 18;
         System.out.println(lesserThan.test(20));
+
+        System.out.println("************CONSUMER***************");
+        Consumer<String> display = i -> System.out.println(i);
+        display.accept("Consumer Interface");
+
     }
 }
