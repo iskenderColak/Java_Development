7e8985204c00a72bd1dfeca048499ade5dfd99fc
Iskender Colak <iclk84@gmail.com>
2022-08-19 16:44:06 +0300

    Double colon operators

--- m4-double_colon_operators/src/main/java/com/icolak/Main.java
+++ m4-double_colon_operators/src/main/java/com/icolak/Main.java
@@ -1,4 +1,27 @@
 package com.icolak;
 
+import java.util.function.BiFunction;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Calculate sum = (x, y) -> System.out.println(x + y);
+        Calculate s1 = (x, y) -> Calculator.findSum(x, y);
+
+        //Reference to a Static Method
+        Calculate s2 = Calculator::findSum;
+        s2.calculate(10, 20);
+
+        //Reference to a instance method
+        Calculator obj = new Calculator();
+        Calculate s3= obj::findMultiply;
+        Calculate s4 = new Calculator()::findMultiply;
+
+        BiFunction<String, Integer, String> func = (str, i) -> str.substring(i);
+        BiFunction<String, Integer, String> func2 = String::substring;
+        System.out.println(func2.apply("iskender", 4)); // i s k e n d e r  ==> n d e r
+                                                            //  0 1 2 3 4 5 6 7      4 5 6 7
+
+    }
 }
